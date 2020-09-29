3db93466d5f83afa4ca70528e134b1809a256cd5
Tony Watkins <tony@llifion.net>
2020-09-29 15:53:25 +0100

    Fix the initial broken implementation to generate a simple subtotal based on the total price of the cart

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleUI
         {
             PopulateCartWithDemoData();
 
-            
+            Console.WriteLine($"The total for the cart is {cart.GenerateTotal():C2}");
 
             Console.WriteLine();
             Console.Write("Please press any key to exit the application...");
