085ae6326acf0bf7035733ab26e6e962624c5c1b
Tony Watkins <tony@llifion.net>
2020-09-30 16:13:13 +0100

    Slightly contrived example with three delegates passed into out generate total method - usually one delegate would probably be passed in. Note use of anonymous functions with the thick arrow syntax.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -17,6 +17,19 @@ namespace ConsoleUI
 
             Console.WriteLine($"The total for the cart is {cart.GenerateTotal(SubTotalAlert, CalculateLeveledDiscount, AlertUser):C2}");
 
+            decimal total = cart.GenerateTotal((subTotal) => Console.WriteLine($"The subtotal for cart 2 is {subTotal:C2}"),
+                (products, subTotal) => { 
+                    if (products.Count > 3)
+                    {
+                        return subTotal * 0.5M;
+                    }
+                    else
+                    {
+                        return subTotal;
+                    }
+                },
+                (message) => Console.WriteLine($"Cart 2 Alert: {message}"));
+
             Console.WriteLine();
             Console.Write("Please press any key to exit the application...");
             Console.ReadKey();
